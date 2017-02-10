7ef575ab1dcdcc6d67c54d39a18cc805b54f6c2d
danisluis6 <vuongluis@hotmail.com>
2017-02-10 17:04:33 +0700

    ok

--- app/src/main/java/com/example/enclaveit/multiprocesstech/MainActivity.java
+++ app/src/main/java/com/example/enclaveit/multiprocesstech/MainActivity.java
@@ -1,61 +1,59 @@
 package com.example.enclaveit.multiprocesstech;
 
+import android.support.annotation.IntegerRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.List;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private  String temp = "";
-    private ListView listView;
-    private List<String> data;
-    private String[] tempp;
+    private TextView number1;
+    private TextView number2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        listView = (ListView)this.findViewById(R.id.listView);
-        data = new ArrayList<>();
         /**
          * @author: Lorence
          * - Runable: contain content of work, this is paragraph code to do that.
          * - Thread: Include runable and running runable
          * - Example: Test which thread running ....
          */
-
+        number1 = (TextView)this.findViewById(R.id.number1);
+        number2 = (TextView)this.findViewById(R.id.number2);
 
         /**
          * Example 1: I want to create five thread or small process
          */
-        for(int i = 0; i < 1000; i++){
-            Thread newThread  = new Thread(new MyRunable());
-            newThread.start();
-        }
 
-        // Finish Thread and See Result From It.
-        tempp = temp.split(",");
-        for(int index = 0; index < tempp.length; index++){
-            data.add(tempp[index]);
-        }
-        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,data);
-        listView.setAdapter(adapter);
+        Thread one  = new Thread(new NumberOne());
+        Thread two  = new Thread(new NumberTwo());
+
+        one.start();
+        two.start();
     }
 
-    class MyRunable implements  Runnable{
+    class NumberOne implements  Runnable{
 
         @Override
         public void run() {
             // To do
-            temp += String.valueOf(Thread.currentThread().getName()+" ...start!,");
+            int temp = Integer.parseInt(number1.getText().toString())+1;
+            number1.setText(String.valueOf(temp));
         }
     }
+
+    class NumberTwo implements  Runnable{
+
+        @Override
+        public void run() {
+            // To do
+            int temp = Integer.parseInt(number2.getText().toString())+2;
+            number2.setText(String.valueOf(temp));
+        }
+    }
+
 }
+
+// This is a problem for weekend. I hope you have a experience about process problems.
